3e8e3dbb0c4db8e0838a43781308140037898ee4
Keke <kekearif@googlemail.com>
2023-02-10 22:22:06 +0800

    added first post

--- src/config.ts
+++ src/config.ts
@@ -1,10 +1,10 @@
 import type { SocialObjects } from "./types";
 
 export const SITE = {
-  website: "https://astro-paper.pages.dev/",
-  author: "Sat Naing",
-  desc: "A minimal, responsive and SEO-friendly Astro blog theme.",
-  title: "AstroPaper",
+  website: "https://kekearif.com/",
+  author: "Keke Arif",
+  desc: "Personal website of Keke Arif, an iOS Engineer based in Taiwan.",
+  title: "KekeArif",
   ogImage: "astropaper-og.jpg",
   lightAndDarkMode: true,
   postPerPage: 3,
@@ -20,7 +20,7 @@ export const LOGO_IMAGE = {
 export const SOCIALS: SocialObjects = [
   {
     name: "Github",
-    href: "https://github.com/satnaing/astro-paper",
+    href: "https://github.com/kekearif",
     linkTitle: ` ${SITE.title} on Github`,
     active: true,
   },
@@ -28,17 +28,17 @@ export const SOCIALS: SocialObjects = [
     name: "Facebook",
     href: "https://github.com/satnaing/astro-paper",
     linkTitle: `${SITE.title} on Facebook`,
-    active: true,
+    active: false,
   },
   {
     name: "Instagram",
     href: "https://github.com/satnaing/astro-paper",
     linkTitle: `${SITE.title} on Instagram`,
-    active: true,
+    active: false,
   },
   {
     name: "LinkedIn",
-    href: "https://github.com/satnaing/astro-paper",
+    href: "https://linkedin.com/in/kekearif",
     linkTitle: `${SITE.title} on LinkedIn`,
     active: true,
   },
